e28ed90baa267bb497f4f07367170112af64b08d
priyanshu1019 <goutampriyanshu27@gmail.com>
2024-06-10 13:56:11 +0530

    var examples

---
+++ app/src/main/java/com/example/codelab1nusevariables/nusevariables/updateVariable2.kt
@@ -0,0 +1,9 @@
+package com.example.codelab1nusevariables.nusevariables
+
+fun main() {
+    var cartTotal = 0
+    println("Total: $cartTotal")
+
+    cartTotal = 20
+    println("Total: $cartTotal")
+}
