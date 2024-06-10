b12ad6d3506a4c60e273b4035597c605acad7623
priyanshu1019 <goutampriyanshu27@gmail.com>
2024-06-10 13:52:48 +0530

    different way of variable declaration

---
+++ app/src/main/java/com/example/codelab1nusevariables/nusevariables/third.kt
@@ -0,0 +1,7 @@
+package com.example.codelab1nusevariables.nusevariables
+
+fun main() {
+    val unreadCount = 5
+    val readCount = 100
+    println("You have ${unreadCount + readCount} total messages in your inbox.")
+}
