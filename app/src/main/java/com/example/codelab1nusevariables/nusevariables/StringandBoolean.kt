2879525c934b2dca1f036667d06946a4a22102d6
priyanshu1019 <goutampriyanshu27@gmail.com>
2024-06-10 13:59:20 +0530

    Boolean and String data type examples

---
+++ app/src/main/java/com/example/codelab1nusevariables/nusevariables/StringandBoolean.kt
@@ -0,0 +1,10 @@
+package com.example.codelab1nusevariables.nusevariables
+
+fun main() {
+    val nextMeeting = "Next meeting: "
+    val date = "January 1"
+    val reminder = nextMeeting + date + " at work"
+    println(reminder)
+    val notificationsEnabled: Boolean = true
+    println(notificationsEnabled)
+}
